00c024dcd86da6caca3245ad098fb244b59449ba
Gabriel Mariani <gmariani405@gmail.com>
2024-06-28 16:36:35 -0400

    Revoked and moved API key to .env

--- index.php
+++ index.php
@@ -3470,7 +3470,7 @@ parsed.extensions.subject_alt_name.dns_names: mariani.life*/
             });
         </script>
     <?php } ?>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVLQb71FZZWez5fgLsKSz1ZGfgQYxrPk4&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=<?= $_ENV['GOOGLE_MAPS'] ?>&callback=initMap"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
